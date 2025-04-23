226c9c36714a579cace9d25746f15dc027904e0a
luizfelipew <luizfelipewendt@gmail.com>
2025-04-23 11:17:06 -0300

    Criando lógica para realizar pagamento com SUCESSO

--- saga-orquestrador/payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
+++ saga-orquestrador/payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
@@ -3,7 +3,9 @@ package br.com.microservices.orchestrated.paymentservice.core.service;
 
 import br.com.microservices.orchestrated.paymentservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.paymentservice.core.dto.Event;
+import br.com.microservices.orchestrated.paymentservice.core.dto.History;
 import br.com.microservices.orchestrated.paymentservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.paymentservice.core.enums.EPaymentStatus;
 import br.com.microservices.orchestrated.paymentservice.core.model.Payment;
 import br.com.microservices.orchestrated.paymentservice.core.producer.KafkaProducer;
 import br.com.microservices.orchestrated.paymentservice.core.repository.PaymentRepository;
@@ -12,6 +14,10 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+import static br.com.microservices.orchestrated.paymentservice.core.enums.ESagaStatus.SUCCESS;
+
 @Slf4j
 @Service
 @AllArgsConstructor
@@ -19,6 +25,7 @@ public class PaymentService {
 
     private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
     private static final Double REDUCE_SUM_VALUE = 0.0;
+    private static final Double MINIMUM_AMOUNT_VALUE = 0.1;
 
     private final JsonUtil jsonUtil;
     private final KafkaProducer producer;
@@ -28,6 +35,10 @@ public class PaymentService {
         try {
             checkCurrentValidation(event);
             createPendingPayment(event);
+            var payment = findByOrderIdAndTransactionId(event);
+            validateAmount(payment.getTotalAmount());
+            changePaymenteToSuccess(payment);
+            handleSuccess(event);
         } catch (Exception ex) {
             log.error("Error trying to make payment: ", ex);
         }
@@ -79,6 +90,41 @@ public class PaymentService {
     private void setEventAmountItems(Event event, Payment payment) {
         event.getPayload().setTotalAmount(payment.getTotalAmount());
         event.getPayload().setTotalItems(payment.getTotalItems());
+    }
+
+    private void changePaymenteToSuccess(Payment payment) {
+        payment.setStatus(EPaymentStatus.SUCCESS);
+        save(payment);
+    }
+
+    private void validateAmount(double amount) {
+        if (amount < MINIMUM_AMOUNT_VALUE) {
+            throw new ValidationException("The minimum amount available is 0.1 "
+                    .concat(MINIMUM_AMOUNT_VALUE.toString()));
+        }
+    }
+
+    private void handleSuccess(final Event event) {
+        event.setStatus(SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Payment realized successfully.");
+    }
+
+    private void addHistory(final Event event, final String message) {
+        var history = History.builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        event.addToHistory(history);
+    }
+
+    private Payment findByOrderIdAndTransactionId(Event event) {
+        return paymentRepository
+                .findByOrderIdAndTransactionId(event.getPayload().getId(), event.getTransactionId())
+                .orElseThrow(() -> new ValidationException("Payment not found by OrderID and TransactionID"));
     }
 
     private void save(final Payment payment) {
